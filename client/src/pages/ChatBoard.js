a3790768ad2decf6edb107f6422461778c2e487f
manaf-abdul <howtotech000@gmail.com>
2023-03-29 18:12:30 +0530

    test

--- client/src/pages/ChatBoard.js
+++ client/src/pages/ChatBoard.js
@@ -4,7 +4,24 @@ const ChatBoard = () => {
   return (
     <>
     <div className="parent">
-    <div className='about_and_setting'>About You and Setting</div>
+    <div className='about_and_setting'>About You and Setting
+    <div className="user_profile">
+        This is User profile
+    </div>
+
+    <div className="message_icon">
+        <p>Messages</p>
+    </div>
+
+    <div className="groups">
+        <p>Groups</p>
+    </div>
+    <div className="contacts">
+        <p>Contacts</p>
+    </div>
+
+    <div className="profile"></div>
+    </div>
     <div className='coversation'>Conversation</div>
     <div className='chat_dashboard'>chat dashboard</div>
     <div className='chat_info'>chat info</div>
